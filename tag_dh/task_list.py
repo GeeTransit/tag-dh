ae93dbbc6837bceb80e74901a0478b403a50c35e
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-05-02 16:25:21 -0400

    smol modifications

--- tag_dh/task_list.py
+++ tag_dh/task_list.py
@@ -9,9 +9,9 @@ bp = Blueprint('task_list', __name__)
 
 @bp.route('/signup', methods=('GET','POST'))
 def signup():
-	if request.method == "POST":
+    if request.method == "POST":
 
-	return render_template('task_list/signup.html'))
+    return render_template('task_list/signup.html'))
 
 @bp.route('/login', methods=('GET','POST'))
 def login():
