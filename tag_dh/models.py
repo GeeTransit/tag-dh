5e451b75f7401ab31500e976a9183404076921c8
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-05-02 16:07:20 -0400

    changed pass to pwrd in models.py

--- tag_dh/models.py
+++ tag_dh/models.py
@@ -10,4 +10,4 @@ class Task(db.Model):
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Text(), nullable=False)
-    pass = db.Column(db.Text(), nullable=False)
+    pwrd = db.Column(db.Text(), nullable=False)
