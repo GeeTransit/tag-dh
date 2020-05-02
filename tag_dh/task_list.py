81e751b9b78e551d1fb9d40a19f9c2049c787852
GeeTransit <geetransit@gmail.com>
2020-05-02 14:06:56 -0400

    Fix indentation

--- tag_dh/task_list.py
+++ tag_dh/task_list.py
@@ -12,19 +12,19 @@ def login():
     if session.get("validUser", False):
         return redirect(url_for('task_list.index'))
 
-	session['validUser'] = True
-	return render_template('task_list/login.html')
+    session['validUser'] = True
+    return render_template('task_list/login.html')
 
 
 @bp.route('/logout', methods=('GET','POST'))
 def logout():
-	session.pop('validUser', None)
-	return render_template('task_list/logout.html')
+    session.pop('validUser', None)
+    return render_template('task_list/logout.html')
 
 
 @bp.route('/', methods=('GET', 'POST'))
 def index():
-	if not session.get('validUser', False):
+    if not session.get('validUser', False):
         return redirect(url_for('task_list.login'))
 
     if request.method == 'POST':
