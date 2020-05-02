8178081f46a96a37bf34a2b8b98382322a15de85
GeeTransit <geetransit@gmail.com>
2020-05-02 01:24:26 -0400

    Big change to Flask

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: flask db upgrade; gunicorn task_list:'create_app()'
---
+++ tag_dh/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_key'),
+        SQLALCHEMY_DATABASE_URI = os.environ.get(
+            'DATABASE_URL',
+            'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite')
+        ),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import models
+    from . import task_list
+    app.register_blueprint(task_list.bp)
+
+    return app
