ccdca328a2936328c33da5df8db685a13fd76a87
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2020-06-09 16:48:56 -0400

    Revert "Update models.py"
    This reverts commit 41eb5e0a5eaf55287804c785fbb5262b21dd4795.

--- tag_dh/models.py
+++ tag_dh/models.py
@@ -83,3 +83,49 @@ class Badge(db.Model):
 
     def __repr__(self):
         return f'<Badge: id={self.id!r} name={self.name!r}>'
+
+
+# Links between Post, Clash, and Account
+
+'''   unused
+class PostClashLink(db.Model):
+    __tablename__ = 'postclashlink'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    clash_id = db.Column(db.Integer, db.ForeignKey('clash.id'), primary_key=True)
+    relationType = db.Column(db.String(24))
+    post = db.relationship("Post", back_populates="clashes")
+    clash = db.relationship("Clash", back_populates="posts")
+
+class PostAccountLink(db.Model):
+    __tablename__ = 'postaccountlink'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), primary_key=True)
+    relationType = db.Column(db.String(24))
+    post = db.relationship("Post", back_populates="accounts")
+    account = db.relationship("Account", back_populates="posts")
+
+class ClashAccountLink(db.Model):
+    __tablename__ = 'clashaccountlink'
+
+    clash_id = db.Column(db.Integer, db.ForeignKey('clash.id'), primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), primary_key=True)
+    relationType = db.Column(db.String(24))
+    clash = db.relationship("Clash", back_populates="accounts")
+    account = db.relationship("Account", back_populates="clashes")
+'''
+
+'''
+class Clash(db.Model):
+    __tablename__ = 'clash'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+
+    posts = db.relationship("PostClashLink", back_populates="clash")
+    accounts = db.relationship("ClashAccountLink", back_populates="clash")
+
+    def __repr__(self):
+        return f'<Clash: id={self.id!r} name={self.name!r}>'
+'''
